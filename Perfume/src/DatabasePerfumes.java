506c9ab472e6d3c18b1506f7fb021192f332d441
SuzannaO <ostrovskaya.s3@gmail.com>
2021-11-03 22:50:12 +0400

    Initial commit

--- Perfume/src/DatabasePerfumes.java
+++ Perfume/src/DatabasePerfumes.java
@@ -1,42 +1,33 @@
 import java.sql.*;
 
 public class DatabasePerfumes {
-    static final String DB_URL = "jdbc:mysql://?";
-    static final String User = "?";
-    static final String Password = "?";
+    static final String DB_URL = "jdbc:mysql://group4database.ckfcq92zr1jy.eu-west-2.rds.amazonaws.com/Perfumes";
+    static final String User = "admin";
+    static final String Password = "Group4database";
     static Connection conn = null;
     static Statement stmt = null;
 
     public static void main(String[] args) {
         try {
-            //Return connection instance
-            System.out.println("Connecting to database...");
+
+            System.out.println("Connecting to Perfumes database...");
             conn = DriverManager.getConnection(DB_URL, User, Password);
             System.out.println("Creating statement...");
             stmt = conn.createStatement();
 
-            selectExample();
-            addExample();
-            insertExample();
-            updateExample();
-            deleteExample();
-            insertWithValue("Chanel", "Chance", "Eau de Toilette Spray", 50, "Fresh");
+            selecting();
+            adding();
+            inserting();
+            updating();
+            deleting();
+            insertingValue("Chanel", "Chance", "Eau de Toilette Spray", 50, "Fresh");
         } catch (SQLException sqlException) {
             System.out.println("Error:" + sqlException.getMessage());
         }
-        // finally {
-        // try {
-        //    stmt.close();
-        //    conn.close();
-        //  }
-        // catch (SQLException ex)
-        //  {
 
-        // }
-        // }
     }
 
-    public static void selectExample() throws SQLException {
+    public static void selecting() throws SQLException {
         String sql = "SELECT * FROM Perfumes";
         ResultSet rs = stmt.executeQuery(sql);
 
@@ -47,30 +38,30 @@ public class DatabasePerfumes {
         rs.close();
     }
 
-    public static void addExample() throws SQLException {
+    public static void adding() throws SQLException {
         String sql = "ALTER TABLE Perfumes ADD Gender VARCHAR(15)";
         System.out.println("Added: " + stmt.executeUpdate(sql));
     }
 
-    public static void insertExample() throws SQLException {
+    public static void inserting() throws SQLException {
         String sql = "INSERT INTO Perfumes (Brand, Title, Type, Capacity, Category) VALUES ('Christian Dior','Miss Dior',‘Eau de Parfum’, 100,'Floral')";
         System.out.println("Inserted: " + stmt.executeUpdate(sql));
     }
 
-    public static void updateExample() throws SQLException {
+    public static void updating() throws SQLException {
         String sql = "UPDATE Perfumes\n" +
                 "SET Title = 'Coco Mademoiselle'\n" +
                 "WHERE Brand = 'Chanel'";
         System.out.println("Updated: " + stmt.executeUpdate(sql));
     }
 
-    public static void deleteExample() throws SQLException {
+    public static void deleting() throws SQLException {
         String sql = "DELETE FROM Perfumes WHERE Title='Bloom'";
         System.out.println("Deleted: " + stmt.executeUpdate(sql));
     }
 
-    public static void insertWithValue(String Brand, String Title, String Type, int Capacity,
-                                       String Category) throws SQLException {
+    public static void insertingValue(String Brand, String Title, String Type, int Capacity,
+                                      String Category) throws SQLException {
         String sql = "INSERT INTO Perfumes (Brand, Title, Type, Capacity, Category) VALUES (+" +
                 Brand + ",'" + Title + "'," + Type + ",'" + Capacity + "','" + Category + "')";
         stmt.execute(sql);
